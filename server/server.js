b163307a33baccff6fcfc8c7a48c6713b9325533
Luciano <luciano.buhler@navomi.com>
2018-10-01 15:46:25 -0300

    Add connect and disconnect avents handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+ // resgister an event Listener
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', (socket) => {
+    console.log('User was disconnected');
+  });
+});
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); // config the middleware
 
 // use is a method to configure the middleware used by the routes of the Express HTTP server object
 // app.use(bodyParser.json());
@@ -25,7 +38,7 @@ app.get('/index', (req, res) => {
 
 // inserted to solve problems with the automatized test
 if(!module.parent) {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Started up at port ${port}`);
   });
 };
